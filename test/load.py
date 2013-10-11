eecf5ac1fa61357b95f5db60de25c9ed3937b9b9
wyy <wuyanyi09@gmail.com>
2013-10-11 02:06:17 -0700

    modify testing

--- test/load.py
+++ test/load.py
@@ -4,20 +4,21 @@ import urllib2
 import threading
 from Queue import Queue
 from time import sleep
+import sys
 
 # 性能测试页面
-PERF_TEST_URL = "http://10.2.66.38/?yyid=-1&suv=1309231700203264&callback=xxxxx"
+#PERF_TEST_URL = "http://10.2.66.38/?yyid=-1&suv=1309231700203264&callback=xxxxx"
+URLS = [line for line in open(sys.argv[1], "r")]
 
 # 配置:压力测试
-#THREAD_NUM = 10            # 并发线程总数
-#ONE_WORKER_NUM = 500       # 每个线程的循环次数
-#LOOP_SLEEP = 0.01      # 每次请求时间间隔(秒)
+THREAD_NUM = 10            # 并发线程总数
+ONE_WORKER_NUM = 500       # 每个线程的循环次数
+LOOP_SLEEP = 0.01      # 每次请求时间间隔(秒)
 
 # 配置:模拟运行状态
-THREAD_NUM = 10        # 并发线程总数
-ONE_WORKER_NUM = 10      # 每个线程的循环次数
-LOOP_SLEEP = 0        # 每次请求时间间隔(秒)
-
+#THREAD_NUM = 10        # 并发线程总数
+#ONE_WORKER_NUM = 10      # 每个线程的循环次数
+#LOOP_SLEEP = 0        # 每次请求时间间隔(秒)
 
 
 # 出错数
@@ -25,12 +26,12 @@ ERROR_NUM = 0
 
 
 #具体的处理函数，负责处理单个任务
-def doWork(index):
+def doWork(index, url):
     t = threading.currentThread()
     #print "["+t.name+" "+str(index)+"] "+PERF_TEST_URL
 
     try:
-        html = urllib2.urlopen(PERF_TEST_URL).read()
+        html = urllib2.urlopen(url).read()
     except urllib2.URLError, e:
         print "["+t.name+" "+str(index)+"] "
         print e
@@ -46,7 +47,7 @@ def working():
     i = 0
     while i < ONE_WORKER_NUM:
         i += 1
-        doWork(i)
+        doWork(i, URLS[i % len(URLS)])
         sleep(LOOP_SLEEP)
 
     print "["+t.name+"] Sub Thread End"
@@ -76,7 +77,7 @@ def main():
 
     t2 = time.time()
     print "========================================"
-    print "URL:", PERF_TEST_URL
+    #print "URL:", PERF_TEST_URL
     print "任务数量:", THREAD_NUM, "*", ONE_WORKER_NUM, "=", THREAD_NUM*ONE_WORKER_NUM
     print "总耗时(秒):", t2-t1
     print "每次请求耗时(秒):", (t2-t1) / (THREAD_NUM*ONE_WORKER_NUM)
