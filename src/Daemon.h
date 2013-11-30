976720d99f150dabc9ccda42b98a44e49814f945
wyy <wuyanyi09@gmail.com>
2013-11-30 16:35:36 +0800

    merging daemon.cpp/h into hpp

--- src/Daemon.h
+++ src/Daemon.h
@@ -33,19 +33,153 @@ namespace Husky
             }
             ~Daemon(){};
         public:
-            bool start();
-            bool stop();
+            bool start()
+            {
+                string masterPidStr = loadFile2Str(m_pidFile);
+                int masterPid = atoi(masterPidStr.c_str());
+                if(masterPid)
+                {
+                    if (kill(masterPid, 0) == 0)
+                    {
+                        LogError("Another instance exist, ready to quit!");
+                        return false;
+                    }
+                }
+
+                initAsDaemon();
+
+                char buf[64];
+                sprintf(buf, "%d", getpid());
+                if (!WriteStr2File(m_pidFile,buf ,"w"))
+                {
+                    LogFatal("Write master pid fail!");
+                }
+
+                while(true)
+                {
+                    pid_t pid = fork();
+                    if (0 == pid)// child process do
+                    {
+                        signal(SIGUSR1, sigChildHandler);
+                        signal(SIGPIPE, SIG_IGN);
+                        signal(SIGTTOU, SIG_IGN);
+                        signal(SIGTTIN, SIG_IGN);
+                        signal(SIGTERM, SIG_IGN);
+                        signal(SIGINT,  SIG_IGN);
+                        signal(SIGQUIT, SIG_IGN);
+
+                        if(!m_pHandler->init())
+                        {
+                            LogFatal("m_pHandler init failed!");
+                            return false;
+                        }
+                        LogInfo("child process Worker init ok pid[%d]",(int)getpid());
+
+                        if (!m_pHandler->run())
+                        {
+                            LogError("m_pHandler run finish with failure!");
+                            return false;
+                        }
+                        LogInfo("child process finish ok!");
+                        exit(0);
+                    }
+
+                    m_nChildPid=pid;
+                    int status;
+                    pid = wait(&status);
+                    if (!isAbnormalExit(pid, status))
+                    {
+                        LogInfo("child exit normally and Daemon exit");
+                        break;
+                    }
+                }
+                return true;
+            }
+            bool stop()
+            {
+                string masterPidStr = loadFile2Str(m_pidFile);
+                int masterPid = atoi(masterPidStr.c_str());
+                if(masterPid)
+                {
+                    if (kill(masterPid, 0) == 0)
+                    {
+                        kill(masterPid, SIGTERM);
+
+                        int tryTime = 200;        
+                        while (kill(masterPid, 0) == 0 && --tryTime)
+                        {
+                            sleep(1);            
+                        }
+
+                        if (!tryTime && kill(masterPid, 0) == 0)
+                        {
+                            LogError("Time out shutdown fail!");        
+                            return false;
+                        }
+
+                        LogInfo("previous daemon pid[%d] shutdown ok.", masterPid);
+                        return true;
+                    }
+
+                }
+                LogError("Another instance doesn't exist, ready to quit!");
+                return false;
+            }
         public:
-            static void initAsDaemon();
-            static void sigMasterHandler(int sig);
-            static void sigChildHandler(int sig);
-            static bool isAbnormalExit(int pid, int status);
+            static void initAsDaemon()
+            {
+                if (fork() > 0)
+                  exit(0);
+                setsid();
+
+                signal(SIGPIPE, SIG_IGN);
+                signal(SIGTTOU, SIG_IGN);
+                signal(SIGTTIN, SIG_IGN);
+                signal(SIGTERM, sigMasterHandler);
+                signal(SIGINT,  sigMasterHandler);
+                signal(SIGQUIT, sigMasterHandler);
+                signal(SIGKILL, sigMasterHandler);
+            }
+            static bool isAbnormalExit(int pid, int status)
+            {
+                bool bRestart = true;
+                if (WIFEXITED(status)) //exit()or return 
+                {
+                    LogDebug("child normal termination, exit pid = %d, status = %d", pid, WEXITSTATUS(status));
+                    bRestart = false;
+                }
+                else if (WIFSIGNALED(status)) //signal方式退出
+                {
+                    LogError("abnormal termination, pid = %d, signal number = %d%s", pid, WTERMSIG(status),
+#ifdef    WCOREDUMP
+                                WCOREDUMP(status) ? " (core file generated)" : 
+#endif
+                                ""); 
+
+                    if (WTERMSIG(status) == SIGKILL)
+                    {
+                        bRestart = false;
+                        LogError("has been killed by user , exit pid = %d, status = %d", pid, WEXITSTATUS(status));
+                    }
+                }
+                else if (WIFSTOPPED(status)) //暂停的子进程退出
+                {
+                    LogError("child stopped, pid = %d, signal number = %d", pid, WSTOPSIG(status));
+                }
+                else
+                {
+                    LogError("child other reason quit, pid = %d, signal number = %d", pid, WSTOPSIG(status));
+                }
+                return bRestart;
+            }
         private:
-            //static IRequestHandler* m_pHandler;
-            //static ServerFrame m_ServerFrame;
             static int m_nChildPid;
             static IWorkHandler * m_pHandler;
-            static const char* m_pidFile;
+        private:
+            const char* m_pidFile;
+        public:
+            static void sigMasterHandler(int sig);
+            static void sigChildHandler(int sig);
     };
 }
 #endif
