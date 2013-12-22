6464fb549a9934b92950515f65e471d51c615563
wyy <wuyanyi09@gmail.com>
2013-12-22 08:09:01 -0800

    add THttpReqInfo.cpp

--- test/unittest/THttpReqInfo.cpp
+++ test/unittest/THttpReqInfo.cpp
@@ -1,8 +1,8 @@
+#include "src/HttpReqInfo.hpp"
+#include "gtest/gtest.h"
 
-#ifdef UT
-
-using namespace Limonp;
-int main()
+using namespace Husky;
+TEST(HttpReqInfoTest, Test1)
 {
     //string url("http://127.0.0.1/?k1=v1&k2=v2 hh");
     //HashMap<string, string> mp;
@@ -13,9 +13,6 @@ int main()
     reqinfo.load(header);
     reqinfo["CLIENT_IP"] = "11.1.1.1";
     string s;
-    //cout<<reqinfo.GET("%20k")
-    cout<<reqinfo.toString()<<endl;
-    return 0;
+    ASSERT_EQ(s << reqinfo, "{ACCEPT-ENCODING:gzip,deflate,sdch, ACCEPT:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8, HOST:10.109.245.13:11256, CLIENT_IP:11.1.1.1, PATH:/?hehek1=v1&htt, PROTOCOL:HTTP/1.1, METHOD:GET, CONNECTION:keep-alive, ACCEPT-LANGUAGE:zh-CN,zh;q=0.8, USER-AGENT:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36}{hehek1:v1}{}");
 }
 
-#endif
