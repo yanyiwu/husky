bee8648c6d9da1eac7e3eb7f332f937350b91d66
wyy <wuyanyi09@gmail.com>
2013-12-22 22:51:55 -0800

    modify test

--- test/servertest/run_curl.sh
+++ test/servertest/run_curl.sh
@@ -1,10 +1,6 @@
 CURL_RES=curl.res
 TMP=curl.res.tmp
-for i in $(seq 10)
-do
-    echo $i
-    curl -s "http://127.0.0.1:11257" >> $TMP
-done
+curl -s "http://127.0.0.1:11257" >> $TMP
 if diff $TMP $CURL_RES >> /dev/null
 then
     echo "ok";
