fd628d439a7d3afe13e289cbb296a617c8303f79
wyy <wuyanyi09@gmail.com>
2014-09-16 22:47:35 +0800

    interrupt socket receive when header is too long.

--- test/unittest/THttpReqInfo.cpp
+++ test/unittest/THttpReqInfo.cpp
@@ -10,7 +10,7 @@ TEST(HttpReqInfoTest, Test1)
     //cout<<HashMapToString(mp)<<endl;
     const char * header = "GET /?hehek1=v1&htt HTTP/1.1\r\nHost: 10.109.245.13:11256\r\nConnection: keep-alive\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: zh-CN,zh;q=0.8\r\n\r\n";
     HttpReqInfo reqinfo;
-    ASSERT_TRUE(reqinfo.parseHeaders(header, strlen(header)));
+    ASSERT_TRUE(reqinfo.parseHeader(header, strlen(header)));
     reqinfo.set("CLIENT_IP", "11.1.1.1");
     string s;
     ASSERT_TRUE(reqinfo.find("ACCEPT", s));
@@ -21,7 +21,7 @@ TEST(HttpReqInfoTest, Test2)
 {
     const char * header = "POST / HTTP/1.1\r\nHost: 10.109.245.13:11256\r\nConnection: keep-alive\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: zh-CN,zh;q=0.8\r\nContent-Length: 12\r\n\r\nhello world.";
     HttpReqInfo reqinfo;
-    ASSERT_TRUE(reqinfo.parseHeaders(header, strlen(header)));
+    ASSERT_TRUE(reqinfo.parseHeader(header, strlen(header)));
     reqinfo.set("CLIENT_IP", "11.1.1.1");
     string s;
     ASSERT_TRUE(reqinfo.find("ACCEPT", s));
@@ -33,5 +33,5 @@ TEST(HttpReqInfoTest, Test3)
 {
     const char * header = "POST /123 123 HTTP/1.1\r\nHost: 10.109.245.13:11256\r\nConnection: keep-alive\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: zh-CN,zh;q=0.8\r\n\r\nhello world.\r\n";
     HttpReqInfo reqinfo;
-    ASSERT_FALSE(reqinfo.parseHeaders(header, strlen(header)));
+    ASSERT_FALSE(reqinfo.parseHeader(header, strlen(header)));
 }
