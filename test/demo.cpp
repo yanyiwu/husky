73197c981e8b03470e33dcdd099d7fbb831c9934
wyy <wuyanyi09@gmail.com>
2014-09-18 00:37:11 +0800

    update demo.cpp for new string_format

--- test/demo.cpp
+++ test/demo.cpp
@@ -12,12 +12,12 @@ class ReqHandler: public IRequestHandler
             const unordered_map<string, string>& mp = httpReq.getMethodGetMap();
             string mpStr;
             mpStr << mp;
-            string_format(response, "{method:GET, arguments:%s}", mpStr.c_str());
+            response = string_format("{method:GET, arguments:%s}", mpStr.c_str());
             return true;
         }
         virtual bool do_POST(const HttpReqInfo& httpReq, string& response) const
         {
-            string_format(response, "{body:%s}", httpReq.getBody().c_str());
+            response = string_format("{body:%s}", httpReq.getBody().c_str());
             return true;
         }
 };
